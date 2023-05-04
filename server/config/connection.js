14d2512b1b1c5858bba4f1da9b0c4b347ffc3d39
Eric Reyes <ericreyes1109@gmail.com>
2023-05-04 13:00:39 -0500

    trying to deploy

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/google_book_search', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/Book_Search_Engine', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 });
